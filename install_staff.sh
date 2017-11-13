a125ea8756e599364068037d7e6421c0b0e522c9
vladyslav valko <vlad.valko@gmail.com>
2017-11-13 12:53:02 +0200

    purge

--- install_staff.sh
+++ install_staff.sh
@@ -12,7 +12,7 @@ sudo aptitude update &&
 
 sudo aptitude upgrade -y
 
-sudo aptitude install htop iotop iftop -y
+sudo aptitude  purge  htop iotop iftop -y
 
 #sudo mv /etc/localtime /etc/localtime_old &&
 #sudo ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime
