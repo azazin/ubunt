dd3cb184b6f58295e1480ed9bfbfa0e8d87f9e2e
vladyslav valko <vlad.valko@gmail.com>
2017-11-13 12:42:59 +0200

    install_all

--- install_staff.sh
+++ install_staff.sh
@@ -12,10 +12,10 @@ sudo aptitude update &&
 
 sudo aptitude upgrade -y
 
-sudo aptitude install htop iotop iftop  -y
+sudo aptitude install htop iotop iftop mc -y
 #sudo mv /etc/localtime /etc/localtime_old &&
 #sudo ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime
-sudo aptitude install mc -y
+#sudo aptitude install mc -y
 
 
 
