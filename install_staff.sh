738b17e38fc12d359e11cbfb50725813bd3cde61
vladyslav valko <vlad.valko@gmail.com>
2017-11-13 11:45:46 +0200

    localtime

--- install_staff.sh
+++ install_staff.sh
@@ -8,10 +8,13 @@
 #echo     -e "\033[1m Dear useername, please pay attention, this script must me started with superuser privileges\033[0m"
 #pause '"Press ENTER to continue or Ctrl+C to cancel"'
 
-
 sudo aptitude update &&
 
 sudo aptitude upgrade -y
 
-sudo aptitude install htop iotop iftop -y
+sudo aptitude install htop iotop iftop  -y
+sudo mv /etc/localtime /etc/localtime_old &&
+sudo ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime
+
+
 
